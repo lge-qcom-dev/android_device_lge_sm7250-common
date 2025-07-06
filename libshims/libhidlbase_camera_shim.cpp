2a2a9e2febf954370126614f585ca5ec0cb20f19
Han Sol Jin <hansol@hansol.ca>
2025-07-06 11:44:00 -0700

    sm7250-common: add shim to nop configureRpcThreadpool
    This function is changing the threadpool size which is no longer
    allowed on S and above.
    
    Change-Id: Ib739718d771b83a38459b61ea58880f95c563941

---
+++ libshims/libhidlbase_camera_shim.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stddef.h>
+
+namespace android {
+namespace hardware {
+
+extern "C" void _ZN7android8hardware22configureRpcThreadpoolEmb(size_t maxThreads, bool callerWillJoin) {
+    // nop call
+    return;
+}
+
+}  // namespace hardware
+}  // namespace android
