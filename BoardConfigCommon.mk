94ca69a20dd70e880d2ad6f645703018d59464f7
basamaryan <basam.aryan@gmail.com>
2025-07-08 15:51:28 -0400

    sm7250-common: Override kernel BPF version
    Change-Id: I3c581f3120b6258b3b45020c5ba12f6bf5b0ee27

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -124,6 +124,7 @@ TARGET_BOARD_PLATFORM := lito
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
