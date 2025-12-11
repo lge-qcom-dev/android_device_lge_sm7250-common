45c7eaa3c0c487eb48713dd1173127ee516731b4
Michael Bestas <mkbestas@lineageos.org>
2025-12-11 13:30:36 +0200

    sm7250-common: Patch libdpps to depend on libtinyxml2-v34.so
    libtinyxml2 10.1.0 update which landed in BP4A contains ABI incompatible changes which cause runtime crashes.
    
    Change-Id: Icc67da5f754b0bb30dce6192b1e520fdf1349ac5

--- extract-files.py
+++ extract-files.py
@@ -47,6 +47,8 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('class early_hal', 'class core'),
     ('vendor/lib/libwvhidl.so', 'vendor/lib64/libwvhidl.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
+    'vendor/lib64/libdpps.so': blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
 }  # fmt: skip
 
 
